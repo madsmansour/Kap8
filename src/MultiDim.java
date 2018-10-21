4248e98998a5e85c383745165abb6dc5266d371f
madsmansour <42993941+madsmansour@users.noreply.github.com>
2018-10-21 14:32:34 +0200

    testing output

---
+++ src/MultiDim.java
@@ -0,0 +1,11 @@
+public class MultiDim {
+    public static void main(String[] args) {
+
+        int[][] array = {{1,2}, {3,4}, {5,6}};
+        for (int i = array.length - 1; i >= 0; i--) {
+            for (int j = array[i].length - 1; j >= 0; j--)
+                System.out.println(array[i][j] + " ");
+            System.out.println();
+        }
+    }
+}
